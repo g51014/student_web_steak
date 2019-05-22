f2248266a3cb4321ac3b95db2dd2f5d8c604992d
g51014 <a912686931@gmail.com>
2019-05-22 18:56:31 +0800

    add description to cook

--- steak/cook/js/script.js
+++ steak/cook/js/script.js
@@ -1,28 +1,36 @@
 const COOK = [{
         title: 'Rare',
-        content:''
+        content:'邊緣熟，中間75%呈紅色',
+        img: './img/cook_3.jpg'
     },
     {
         title: 'Medium Rare',
-        content:''
+        content:'邊緣熟，中間50%紅色',
+        img: './img/cook_5.jpg'
     },
     {
         title: 'Medium',
-        content:''
+        content:'邊緣熟，中心25%呈粉色',
+        img: './img/cook_7.jpg'
     },
     {
         title: 'Well Done',
-        content:''
+        content:'通體呈熟褐色',
+        img: './img/cook_10.jpg'
     }]
 
 
 var topicOption = () => {
     var option = '';
     for(var i = 0; i < COOK.length; i++) {
-        option = option + "<li><button "+ "id=option-"+ (i+1) +"><p>" + COOK[i].title +"</p></button></li>";
+        option = option + "<li><button "+ "id=option-"+ i +"><p>" + COOK[i].title +"</p></button></li>";
     }
-    console.log(option);
     return option;
+}
+
+var content = (id) => {
+    return "<p>"+COOK[id].content+"</p>"+"<a href = '#description'><i class='fa fa-sort-down sort-down-icon'></i></a>"+
+    "<img src ='"+COOK[id].img+"'>";
 }
 
 $(document).ready(
@@ -30,3 +38,19 @@ $(document).ready(
        $('#topic-option').append(this.topicOption());
     }
 );
+
+$(document).on('click',
+    (clickEvenData) => {
+        var target = {
+            option: clickEvenData.originalEvent.path[1].id,
+            icon: clickEvenData.originalEvent.path[0].className,
+        };
+        console.log(clickEvenData.originalEvent.path[0].className);
+        if(target.option.includes('option')) {
+            target.option = parseInt(target.option.split('option-')[1]);
+            $('#description').empty();
+            $('#description').append(this.content(target.option));
+        }
+    }
+);
+
